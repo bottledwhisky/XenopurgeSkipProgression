6a965c4be3e2379d5dc0e48577da15a5e60c48d0
bottledwhisky <215269529+bottledwhisky@users.noreply.github.com>
2026-01-07 02:30:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# XenopurgeSkipProgression
+
+Skip the command progression reward screen after you have maxed everything out.
+
+A MelonLoader mod for Xenopurge.
+
+![screenshot](screenshots/screenshot.png)
+
+## Features
+
+- Removes the command progression reward screen when you have reached the max ranks and completed all challenges.
+
+## Requirements
+
+- [MelonLoader](https://melonloader.co/)
+
+## Installation
+
+Skip to step 2 if you already have MelonLoader installed.
+
+1. Install MelonLoader
+    a. `<game_directory>` is the directory where the game executable is located. For example, `C:\Program Files (x86)\Steam\steamapps\common\Xenopurge`. If you still cannot find it, right-click the game in your Steam library, select "Manage", then "Browse local files".
+2. Place the mod DLL in `<game_directory>/Mods/`
+3. Restart the game
+
+## Usage
+
+Just play the game as normal. The mod works automatically.
+
+## Notes
+
+- Mac users: MelonLoader only supports Windows and Linux. Wait for Steam Workshop support.
+- Xenopurge uses Mono, not IL2CPP.
---
+++ XenopurgeSkipProgression.cs
@@ -0,0 +1,54 @@
+﻿using HarmonyLib;
+using MelonLoader;
+using SpaceCommander.EndGame;
+using SpaceCommander.UI;
+using System.Linq;
+
+[assembly: MelonInfo(typeof(XenopurgeSkipProgression.XenopurgeSkipProgression), "Xenopurge Skip Progression Screen", "1.0.0", "Felix Hao")]
+[assembly: MelonGame("Traptics", "Xenopurge")]
+namespace XenopurgeSkipProgression
+{
+    public class XenopurgeSkipProgression : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Xenopurge Skip Progression Screen Loaded!");
+        }
+    }
+
+    [HarmonyPatch(typeof(EndGameWindowController))]
+    public class GenerateLocationsPatch
+    {
+        static EndGameResultData _endGameResultData;
+
+        [HarmonyPatch("ShowEndScreen")]
+        [HarmonyPostfix]
+        public static void ShowEndScreen_Prefix(EndGameWindowController __instance, EndGameResultData endGameResultData)
+        {
+            _endGameResultData = endGameResultData;
+        }
+
+        [HarmonyPatch("OnShowCommanderProgressionClicked")]
+        [HarmonyPrefix]
+        public static bool OnShowCommanderProgressionClicked_Prefix(EndGameWindowController __instance)
+        {
+            if (_endGameResultData.MustLeaveToSeeLogTerminal || _endGameResultData.RanksGained.Any() || _endGameResultData.ChallengesCompleted.Any() || _endGameResultData.HasUnlockedDifficulty || _endGameResultData.HasUnlockedVariant || _endGameResultData.NextVariant != null)
+            {
+                return true;
+            }
+
+            var _endGameWindowView = AccessTools.Field(typeof(EndGameWindowController), "_endGameWindowView").GetValue(__instance) as EndGameWindowView;
+            _endGameWindowView.gameObject.SetActive(false);
+
+            if (_endGameResultData.IsVictory)
+            {
+                AccessTools.Method(typeof(EndGameWindowController), "OnGoToPartyCustmizationButtonClicked").Invoke(__instance, null);
+            }
+            else
+            {
+                AccessTools.Method(typeof(EndGameWindowController), "OnExitButtonClicked").Invoke(__instance, null);
+            }
+            return false;
+        }
+    }
+}
